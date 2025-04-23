215ff0aac8e9e56a236ccb62fb26eccc09e343fa
Benedikt Veith <benedikt.veith@gmail.com>
2025-04-23 19:52:02 +0200

    [Pipeline] Added strikethrough detection & cleaning

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from pipelines.cv_pipeline import CVPipeline
 from modules.red_remover import RedRemover
 from modules.horizontal_cutter import HorizontalCutter
 from modules.horizontal_cutter_line_detect import HorizontalCutterLineDetect
+from modules.strikethrough_cleaner import StrikeThroughCleaner
 
 from modules.line_cropper import LineCropper
 from modules.text_recognizer import TextRecognizer
@@ -12,10 +13,11 @@ from modules.text_recognizer import TextRecognizer
 pipeline = CVPipeline()
 pipeline.add_stage(RedRemover(debug=True))
 pipeline.add_stage(HorizontalCutterLineDetect(debug=True))
+pipeline.add_stage(StrikeThroughCleaner(debug=True))
 pipeline.add_stage(LineCropper(debug=True))
 pipeline.add_stage(TextRecognizer(debug=True))
 
-input_image_path = os.path.join("data", "input", "image.png")
+input_image_path = os.path.join("data", "input", "image_pdf.pdf")
 output_text_file = os.path.join("data", "output", "recognized_texts.txt")
 
 result = pipeline.run_and_save_text(input_image_path, output_text_file)
