2683f7190f456101068c131d3fc8b92fb8141551
Benedikt Veith <benedikt.veith@gmail.com>
2025-05-25 13:25:16 +0200

    Create schulbuch dictionary

--- school_books/create_dictionary.py
+++ school_books/create_dictionary.py
@@ -17,13 +17,33 @@ def word_frequencies_from_file(file_path):
         words = cleaned_text.split()
 
         for word in words:
+            if word.isdigit():
+                continue
+
             word_freq[word] += 1
 
     return dict(word_freq)
 
-file_path = 'Pädagogik_Psychologie_Band_1.txt'
+file_path = 'Pädagogik_Psychologie_Band_3.txt'
 frequenzen = word_frequencies_from_file(file_path)
 
-for wort, freq in sorted(frequenzen.items(), key=lambda x: x[1], reverse=True)[:10]:
-    print(f"{wort}: {freq}")
+print(frequenzen)
+with open('../models/symspell/de-100k.txt', encoding="utf-8") as f:
+    for line in f:
+        parts = line.strip().split()
+        if len(parts) >= 2:
+            word = clean_text(parts[0])
+            if word.isdigit():
+                continue
+            
+            freq = int(parts[1])
+
+            if frequenzen.get(word, None) is None:
+                frequenzen[word] = freq
+            else:
+                frequenzen[word] = max(frequenzen[word], 0) + freq
+
+with open('../models/symspell/de-100k_schulbuch.txt', "w", encoding="utf-8") as out:
+    for word in sorted(frequenzen):
+        out.write(f"{word} {frequenzen[word]}\n")
 
